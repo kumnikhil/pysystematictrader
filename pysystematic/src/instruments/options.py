0dad6574db4064d83222cffadaf0d0ee98c0d2f0
kumnikhil <kumar.nikhil.217@gmail.com>
2025-05-27 19:42:18 +0530

    option instrument added

---
+++ pysystematic/src/instruments/options.py
@@ -0,0 +1,60 @@
+from pydantic import BaseModel, Field, field_validator, computed_field, constr # type: ignore
+from datetime import date
+from typing import Literal, Union, Optional
+
+
+class OptionObj(BaseModel):
+    underlying_product_code: Optional[str]
+    product_code: Optional[str]
+    expiry_date: Union[str, date]
+    contract_date: Union[str, date]
+    calc_date: Union[str, date]
+    exercise_style: constr(to_upper = True) = Literal['EUROPEAN', 'AMERICAN' ]
+    option_type: constr(to_upper = True) = Literal['PE', 'CE', 'CALL', 'PUT', 'C', 'P']
+    @field_validator('expiry_date', mode = 'before')
+    @classmethod
+    def parse_expiry_date(cls, v):
+        if isinstance(v, str):
+            try:
+                return date.fromisoformat(v)
+            except ValueError:
+                raise ValueError(f"expiry_date must if passed as string must be in YYYY-MM-DD format, received- {v}")
+    @field_validator('contract_date', mode='before')
+    @classmethod
+    def parse_contract_date(cls, v):
+        if isinstance(v,str):
+            try:
+                return date.fromisoformat(v)
+            except ValueError:
+                raise ValueError(f"contract_date must if passed as string must be in YYYY-MM-DD format, received- {v}")
+    @field_validator('calc_date', mode='before')
+    @classmethod
+    def parse_calc_date(cls, v):
+        if isinstance(v,str):
+            try:
+                return date.fromisoformat(v)
+            except ValueError:
+                raise ValueError(f"calc_date must if passed as string must be in YYYY-MM-DD format, received- {v}")
+
+class OptionMarketData(BaseModel):
+    instrument_def: OptionObj
+    underlying_price: float =  Field(...,description="Price of the underlying asset")
+    strike: float = Field(...,description="Strike price of the option")
+    premium: float = Field(...,description="Market price of the option")
+    option_type: Literal["call", "put", 'c','p','ce','pe'] = Field(default="call", description="Type of option (call or put)")
+    implied_volatility: float =  Field(gt=0.0, description ="Implied vol of the option")
+    @computed_field
+    @property
+    def tte(self) -> float:
+        return (self.instrument_def.expiry_date - self.instrument_def.calc_date).days / 365
+    
+if __name__ == "__main__":
+    inst ={'underlying_product_code':'fdsfsdfsd','product_code':'werewrew', 'expiry_date':'2025-10-22', 'contract_date':'2025-10-01', 'calc_date':'2025-05-27', 'exercise_style':'EUROPEAN', 'option_type':'c'}
+    obj_inst =  OptionObj(**inst)
+    data = {'instrument_def':obj_inst, 'underlying_price':103.4,'strike':100., 'premium':3.6, 'option_type':'c', 'implied_volatility':0.37}
+    option_data =  OptionMarketData(**data)
+    print(f"Time to expiry: {option_data.tte}")
+    print(option_data.instrument_def.expiry_date)
+    print(option_data.instrument_def.calc_date)
+    print(option_data.instrument_def.exercise_style)
+    print(option_data.instrument_def.option_type)
---
+++ pysystematic/src/utils/utils_kite.py
@@ -0,0 +1,70 @@
+from kiteconnect import KiteConnect
+import json 
+import os 
+import pandas as pd
+import polars as pl
+import datetime as dt
+from pydantic import BaseModel, Field
+from typing import Union, Optional, List
+import logging
+logger = logging.getLogger(__name__)
+
+class Kite(object):
+    def __init__(self, session_file = "./appdata/api_creds/session_key.json"):
+        self.kite = KiteConnect(api_key=os.getenv("API_KEY"))
+        if not os.path.isfile(session_file):
+            raise FileNotFoundError("Session token file for kiteconnect not found.")
+        with open(session_file,'r') as f:
+            self.sesion_token = json.load(f)['session_token']
+        self.kite.generate_session(self.sesion_token, api_secret=os.getenv("API_SECRET"))
+        self.instruments_df = pd.DataFrame.from_records(self.kite.instruments())
+
+    def get_historical_data(self, instrument_code, start_ts, end_ts, frequency, minutes_ctr=None):
+        if frequency.lower() not in ['day', 'minute']:
+            raise AttributeError(f"Unknown value of the frequency- expected day / minute. Got {frequency}")
+        hist_data_list = None
+        if frequency=='minute' and isinstance(minutes_ctr, int):
+            if minutes_ctr<5 and (pd.to_datetime(end_ts) -  pd.to_datetime(start_ts)).days>60:
+                logger.info("For minute level historical data the max history allowed is 60days so changing the start_ts accordingly.")
+                start_ts  = (pd.to_datetime(end_ts) - dt.timedelta(days=60)).strftime("%Y-%m-%d %H:%m:%S")
+                
+            try:
+                hist_data_list = self.kite.historical_data(instrument_code, start_ts,end_ts,f"{minutes_ctr}minute",oi=True)
+            except Exception as e:
+                logger.error(e)
+        elif frequency=='minute' and not isinstance(minutes_ctr, int):
+            try:
+                hist_data_list = self.kite.historical_data(instrument_code, start_ts,end_ts,"5minute",oi=True)
+            except Exception as e:
+                logger.error(e)
+        elif frequency=='day':
+            try:
+                hist_data_list = self.kite.historical_data(instrument_code, start_ts,end_ts,"day",oi=True)
+            except Exception as e:
+                logger.error(e)
+        else:
+            raise RuntimeError("Runtime error encountered.")
+        if isinstance(hist_data_list, list):
+            hist_lz = pl.LazyFrame(hist_data_list).with_columns([
+                pl.col("date").dt.convert_time_zone("Asia/Kolkata"), # convert to datetime
+                pl.col("date").dt.date().alias("trade_date"), # extract date
+                pl.col("date").dt.time().alias("time") # extract time
+            ])
+            return hist_lz.collect()
+        else:
+            return None
+
+    def get_instruments_by_type(self, instrument_type:str):
+        if str(instrument_type).upper() not in self.instruments_df.instrument_type.unique().tolist():
+            raise AttributeError(f"Unknown instrument_type = {instrument_type}")
+        return self.instruments_df[self.instruments_df.instrument_type==str(instrument_type).upper()]
+
+    def get_instruments_by_segment(self, segment:str):
+        if str(segment).upper() not in self.instruments_df.segment.unique().tolist():
+            raise AttributeError(f"Unknown segment = {segment}")
+        return self.instruments_df[self.instruments_df.segment==str(segment).upper()]
+    
+    def __get_VIX__(self):
+        return self.instruments_df[self.instruments_df.name == 'INDIA VIX']
+  
+        
